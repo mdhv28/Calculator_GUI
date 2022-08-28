7774c6be5a7e19ccdf639c5ec7ead7df8315b2a1
Madhava <50883010+mdhv28@users.noreply.github.com>
2022-08-28 16:24:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,38 @@
-# Python program to create a simple GUI
-# calculator using Tkinter
-
-# import everything from tkinter module
 from tkinter import *
 
-# globally declare the expression variable
 expression = ""
 
 
-# Function to update expression
-# in the text entry box
 def press(num):
-	# point out the global expression variable
 	global expression
 
-	# concatenation of string
 	expression = expression + str(num)
 
-	# update the expression by using set method
 	equation.set(expression)
 
 
-# Function to evaluate the final expression
 def equalpress():
-	# Try and except statement is used
-	# for handling the errors like zero
-	# division error etc.
-
-	# Put that code inside the try block
-	# which may generate the error
+	
 	try:
 
 		global expression
 
-		# eval function evaluate the expression
-		# and str function convert the result
-		# into string
+		
 		total = str(eval(expression))
 
 		equation.set(total)
 
-		# initialize the expression variable
-		# by empty string
+		
 		expression = ""
 
-	# if error is generate then handle
-	# by the except block
+	
 	except:
 
 		equation.set(" error ")
 		expression = ""
 
 
-# Function to clear the contents
-# of text entry box
+
 def clear():
 	global expression
 	expression = ""
